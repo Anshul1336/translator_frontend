8f2c4080be5916a5af6503a75f4ce57b957a3e65
Anshul1336 <140470898+Anshul1336@users.noreply.github.com>
2025-03-23 10:46:41 +0530

    Update App.jsx
    changed the api

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ export default function AudioTranslator() {
 
           console.log("📡 Sending audio to backend...");
           try {
-            const response = await fetch("http://127.0.0.1:5000/api/translate", {
+            const response = await fetch("https://translatorbackend-production.up.railway.app/translate", {
               method: "POST",
               body: formData,
             });
